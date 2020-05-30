998f2e487777f898be080210497dc01684fd74ea
Brian Boehm <bri.boehm@gmail.com>
2020-05-30 23:02:12 +0000

    Done.

--- grandma_dup.rb
+++ grandma_dup.rb
@@ -21,3 +21,12 @@ def speak_to_grandma(speak)
     return "NO, NOT SINCE 1938!"
   end
 end
+  
+#   if speak == "I LOVE YOU GRANDMA!".upcase
+#     puts "I LOVE YOU TOO PUMPKIN!"
+#   elsif speak == speak.upcase
+#     puts "NO, NOT SINCE 1938!"
+#   else
+#     puts "HUH?! SPEAK UP, SONNY!"
+#   end
+# end
