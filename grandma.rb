1c395efbb34bd17b756f1eda5eeb519c08714349
beckyrosa3 <63604524+beckyrosa3@users.noreply.github.com>
2020-05-31 14:32:01 -0600

    Delete grandma.rb

--- grandma.rb
+++
@@ -1,8 +0,0 @@
-def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-puts "I LOVE YOU TOO PUMPKIN!"
-end
